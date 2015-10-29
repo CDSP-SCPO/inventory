8664232c8a8f1e52d143ddf9daefa366855aae1c
Anne Lhote <anne.lhote@gmail.com>
2015-10-29 16:26:46 +0100

    Refacto variable name and display end message on stdout

--- daneelolivaw.py
+++ daneelolivaw.py
@@ -14,7 +14,6 @@ path_separator = '/'
 log_folder = 'log'
 log_level = logging.DEBUG
 results_folder = 'results'
-csv_file = results_folder + path_separator + 'daneelolivaw.csv'
 csv_separator = '\t'
 csv_data = ''
 json_data = {}
@@ -32,17 +31,19 @@ def main(recordsbyid) :
 	logging.info('Start')
 	# If specified, open the quality control sheet to list the documents
 	logging.info('Open quality control sheet')
-	if hasControlSheet :
-		with open(controlSheet, 'rb') as csvfile:
+	# Load scanning sheet if any
+	if has_scanning_sheet :
+		with open(control_sheet, 'rb') as csvfile:
 			spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 			t = [l for l in spamreader]
 			recordsbyid = dict(zip(map(operator.itemgetter(0), t), t))
 	# Start inventory on the main folder
-	inventory(inventoryPath, recordsbyid)
+	inventory(inventory_path, recordsbyid)
 	# Write the results into data files
 	writeCsvFile(csv_data)
 	writeJsonFile(json_data)
 	logging.info('End')
+	print 'Everything worked well !\nCSV and JSON files have been generated into \'' + results_folder + '\' folder.'
 
 def inventory(path, recordsbyid) :
 	global id
@@ -50,9 +51,9 @@ def inventory(path, recordsbyid) :
 	global json_data
 	# Iterate over each folder and file from path
 	for file in os.listdir(path) :
-		completePath = os.path.join(path, file)
+		complete_path = os.path.join(path, file)
 		# If it is a file
-		if os.path.isfile(completePath) :
+		if os.path.isfile(complete_path) :
 			# Check that the file name is correct
 			if len(file.split('_')) >= 9 :
 				# Increment the identifier in the inventory
@@ -71,7 +72,7 @@ def inventory(path, recordsbyid) :
 				else :
 					rank = ''
 				extension = file.split('.')[-1]
-				if hasControlSheet :
+				if has_scanning_sheet :
 					# Check if the file was listed in the quality control sheet
 					try :
 						file_data = recordsbyid['_'.join(splitted_file[:9]).split('.')[0]]
@@ -93,13 +94,14 @@ def inventory(path, recordsbyid) :
 				logging.error('File not conforme : ' + path + file)
 		# If it is a folder, launch inventory on it
 		else :
-			inventory(completePath, recordsbyid)
+			inventory(complete_path, recordsbyid)
 
 def writeCsvFile(data) :
 	# Add csv headers
 	csv_headers = ['N° d\'inventaire', 'Chemin', 'Fichier', 'Fonds', 'Sous-fonds', 'Dossier', 'Sous-dossier', 'Langue', 'Sujet', 'Article', 'N° (série)', 'Extension']
 	data = csv_separator.join(csv_headers) + data
 	# Write results into a csv data file
+	csv_file = results_folder + path_separator + sys.argv[0].replace('.py', '.csv')
 	with codecs.open(csv_file, 'w', 'utf8') as f:
 		f.write(data.decode('utf8'))
 	f.close()
@@ -118,7 +120,7 @@ if __name__ == '__main__':
 	if len(sys.argv) < 2 :
 		print 'Arguments error'
 		print 'Correct usage : ' + sys.argv[0] + ' "path/to/folder/to/inventory" "path/to/the/quality/control/sheet.csv"'
-		print 'The second argument ie. the quality control sheet is optional and has to be a csv file'
+		print 'The second argument ie. the digitalization sheet is optional and has to be a csv file'
 	else :
 		# Check that if the command line has a second argument, it is a csv file
 		if len(sys.argv) >= 3 and sys.argv[2][-4:] != '.csv' :
@@ -127,12 +129,12 @@ if __name__ == '__main__':
 		if len(sys.argv) >= 4 and sys.argv[3][-4:] != '.xml' :
 			print 'The third argument ie. the METS file has to be an XML file'
 		else :
-			inventoryPath = sys.argv[1]
+			inventory_path = sys.argv[1]
 			if len(sys.argv) >= 3 :
-				hasControlSheet = 1
-				controlSheet = sys.argv[2]
+				has_scanning_sheet = 1
+				control_sheet = sys.argv[2]
 			else :
-				hasControlSheet = 0
+				has_scanning_sheet = 0
 			# Check that log folder exists, else create it
 			if not os.path.exists(log_folder):
 				os.makedirs(log_folder)
